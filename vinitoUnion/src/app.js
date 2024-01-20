94b65f18051efe1330d3903224885d07cb9cbcf2
Alejandro Vergara <alejandro.vergara@utp.edu.co>
2024-01-20 14:54:24 -0500

    commit inicial

---
+++ vinitoUnion/src/app.js
@@ -0,0 +1,14 @@
+// Correct Component Import
+import ComponentName from 'react-library';
+
+// Component Usage
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <ComponentName />
+      </div>
+    );
+  }
+}
+export default YourApp;
